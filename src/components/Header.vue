6bcf694b5cec00f83ced78b367e1b2f1aa19bb49
Felix <felixrunye@gmail.com>
2021-07-30 09:08:04 +0300

    added different components and made them reusable

---
+++ src/components/Header.vue
@@ -0,0 +1,30 @@
+<template>
+    <header>
+        <h1>{{title}}</h1>
+        <Button text="Add Task" color="green"/>
+    </header>
+</template>
+<script>
+import Button from './Button.vue';
+
+export default {
+    name:'headerComponent',
+    props:{
+        title:{
+            type:String,
+            default: 'Welcome Felix'
+        }
+    },
+    components:{
+        Button
+    }
+}
+</script>
+<style scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+}
+</style>
---
+++ src/components/Tasks.vue
@@ -0,0 +1,12 @@
+<template>
+    <div v-for="task in tasks" :key="task">
+        <h3>{{task.text}}</h3>
+    </div>
+</template>
+<script>
+export default {
+    props:{
+        tasks:Array
+    }
+}
+</script>
